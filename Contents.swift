968bb61bab8add5731ca3f1a5b0b27d2ea4dad46
ricopineda <ricopineda@me.com>
2017-09-04 11:20:46 -0700

    ininital commit

---
+++ Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var type: String = "Rectangle"
+var description: String = "A quadrilateral with four right angles"
+var width: Int = 5
+var height: Double = 10.5
+var area: Double = Double(width * Int(height))
+height += 1
+width += 1
+area = Double(Int(width) * Int(height))
+// Note how you can "interpolate" variables into Strings using the following syntax
+print("The shape is a \(type) or \(description) with an area of \(area)")  
